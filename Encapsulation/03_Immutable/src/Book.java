ef90a6b12273db0cb9c7f4936e85ca4fb53eb2fe
Kirill Alexeichik <Kira_alx@mail.ru>
2022-03-01 13:09:16 +0300

    Added class Book and Product

---
+++ Encapsulation/03_Immutable/src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private final String name;
+    private final String autor;
+    private final int countList;
+    private final int numberISBN;
+
+
+    public Book(String name, String autor, int countList, int numberISBN) {
+        this.name = name;
+        this.autor = autor;
+        this.countList = countList;
+        this.numberISBN = numberISBN;
+    }
+}
+
