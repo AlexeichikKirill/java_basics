8c1222cb358caceca1f25a1e1affe329118f89de
Kirill Alexeichik <Kira_alx@mail.ru>
2022-03-01 23:44:40 +0300

    Changes applied

---
+++ Encapsulation/02_Pojo/src/Table.java
@@ -0,0 +1,35 @@
+public class Table {
+    public int height;
+    public int weight;
+    public String name;
+
+    public Table(int weight, String name) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
