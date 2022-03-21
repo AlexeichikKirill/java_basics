07a033c64c4f2f1ed4272071b484769280819100
Kirill Alexeichik <Kira_alx@mail.ru>
2022-03-21 22:53:41 +0300

    Added transcription NumberCode in Symbol

--- PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -8,5 +8,20 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        char symbolCode;
+        int defoultQantiti = 1040;
+
+        for (symbolCode = 1040; symbolCode <= 1103; symbolCode++) {
+            if (symbolCode != 1104) {
+                System.out.println(defoultQantiti + " - " + symbolCode);
+            }
+            if (defoultQantiti == 1045) {
+                System.out.println("1025 - Ё");
+            }
+            if (defoultQantiti == 1077) {
+                System.out.println("1105 - ё");
+            }
+            defoultQantiti ++;
+        }
     }
 }
