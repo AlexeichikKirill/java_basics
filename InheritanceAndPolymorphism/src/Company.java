949ffc544fdd644cc2fe918612a3742a3fa3a4bc
Kirill Alexeichik <Kira_alx@mail.ru>
2022-05-25 23:01:21 +0300

    Fix 25.5.2022

--- InheritanceAndPolymorphism/src/Company.java
+++ InheritanceAndPolymorphism/src/Company.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Company {
 
-    List<Employee> employees = new ArrayList<>();
+    private List<Employee> employees = new ArrayList<>();
 
     public void hire(Employee employee) {
         this.employees.add(employee);
@@ -16,43 +16,92 @@ public class Company {
         this.employees.remove(employee);
     }
 
-    public static int getIncome() {
+    public int getIncome() {
         return 15_000_000;
     }
 
     public List<Employee> getTopSalaryStaff(int count) {
-        return getFilteredList(count, new Comparator<Employee>() {
+        return getFilteredList(new Comparator<Employee>() {
             @Override
             public int compare(Employee o1, Employee o2) {
                 return o2.getMonthSalary() - o1.getMonthSalary();
             }
-        });
+        }, count);
     }
 
     public List<Employee> getLowestSalaryStaff(int count) {
-        return getFilteredList(count, new Comparator<Employee>() {
+        return getFilteredList(new Comparator<Employee>() {
             @Override
             public int compare(Employee o1, Employee o2) {
                 return o1.getMonthSalary() - o2.getMonthSalary();
             }
-        });
+        }, count);
     }
 
-    private List<Employee> getFilteredList(int count, Comparator<Employee> comparator) {
-        List<Employee> copyList = new ArrayList<>(employees);
-        Collections.sort(copyList, comparator);
+    public List<Employee> getFilteredList(Comparator<Employee> comparator, int count) {
         List<Employee> result = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            result.add(copyList.get(i));
+        if (count < getCountEmployees()) {
+            List<Employee> copyList = new ArrayList<>(employees);
+            Collections.sort(copyList, comparator);
+            for (int i = 0; i < count; i++) {
+                result.add(copyList.get(i));
+            }
         }
         return result;
     }
 
-    public int countEmployees() {
+    public int getCountEmployees() {
         return employees.size();
     }
 
     public List<Employee> getEmployees() {
         return employees;
+    }
+
+    public void printHighestSalaries(Company company, int count) {
+        System.out.println("Список из " + count + " самых высоких зарплат: ");
+        int i = 1;
+        for (Employee employee : company.getTopSalaryStaff(count)) {
+            System.out.println(i++ + " - " + employee.getMonthSalary());
+        }
+    }
+
+    public void printLowestSalaries(Company company, int count) {
+        System.out.println("Список из " + count + " самых низких зарплат: ");
+        int i = 1;
+        for (Employee employee : company.getLowestSalaryStaff(count)) {
+            System.out.println(i++ + " - " + employee.getMonthSalary());
+        }
+    }
+
+    public void hireEmployees(Company company, int countOperator, int countManager, int countTopManager) {
+        for (int i = 0; i < countOperator; i++) {
+            Employee operator = new Operator();
+            company.hire(operator);
+        }
+        for (int i = 0; i < countManager; i++) {
+            Employee manager = new Manager();
+            company.hire(manager);
+        }
+        for (int i = 0; i < countTopManager; i++) {
+            Employee topManager = new TopManager();
+            company.hire(topManager);
+        }
+    }
+
+    public void fireEmployees(Company company, int percentEmployees) {
+        if (percentEmployees > 0 && percentEmployees < 100) {
+            int countEmployees = company.getCountEmployees();
+            double countFire = (countEmployees * (percentEmployees * 0.01));
+            for (int i = 0; i < countFire; i++) {
+                int rnd = (int) (Math.random() * company.getCountEmployees());
+                Employee fireEmployee = company.getEmployees().get(rnd);
+                company.fire(fireEmployee);
+            }
+            System.out.println("Уволено " + (int) countFire + " сотрудников");
+        } else {
+            System.out.println("Введено неверное количество процентов. " +
+                    "Допустимое количество от 0 до 100.");
+        }
     }
 }
--- InheritanceAndPolymorphism/src/TopManager.java
+++ InheritanceAndPolymorphism/src/TopManager.java
@@ -1,8 +1,9 @@
 public class TopManager implements Employee {
     private static final int FIXED_PART = 120_000;
+    private Company company = new Company();
 
     @Override
     public int getMonthSalary() {
-        return FIXED_PART + Company.getIncome() > 10_000_000 ? (int) (FIXED_PART * 1.5) : 0;
+        return FIXED_PART + company.getIncome() > 10_000_000 ? (int) (FIXED_PART * 1.5) : 0;
     }
 }
