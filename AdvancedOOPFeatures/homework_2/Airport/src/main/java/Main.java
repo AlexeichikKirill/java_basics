b4d91cb9090f66177bf9e8b7b84e8a9710afcf36
Kirill Alexeichik <Kira_alx@mail.ru>
2022-06-01 14:34:47 +0300

    Implements method findPlanesLeavingInTheNextTwoHours

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -10,8 +11,20 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
+        Date now = new Date();
+        Date afterTwoHours = new Date();
+        afterTwoHours.setHours(afterTwoHours.getHours() + 2);
 
+        List<Flight> planesLeavingInTheNextTwoHours = new ArrayList<>();
+
+        airport.getTerminals().forEach((i) -> {
+            i.getFlights()
+                    .stream()
+                    .filter(time -> time.getDate().after(now))
+                    .filter(time -> time.getDate().before(afterTwoHours))
+                    .filter(departure -> departure.getType() == Flight.Type.DEPARTURE)
+                    .forEach(flight -> planesLeavingInTheNextTwoHours.add(flight));
+        });
+        return planesLeavingInTheNextTwoHours;
+    }
 }
