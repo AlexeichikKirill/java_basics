f3425433998ffd9e2a9f41d77049b2d67ca9baf1
Kirill Alexeichik <Kira_alx@mail.ru>
2022-07-04 12:40:57 +0300

    Done exercise FileUtils

--- FilesAndNetwork/CountFileSizes/src/main/java/FileUtils.java
+++ FilesAndNetwork/CountFileSizes/src/main/java/FileUtils.java
@@ -1,6 +1,28 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        File folder = new File(path);
+        long sum = 0;
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += calculateFolderSize(file.getAbsolutePath());
+        }
+        return sum;
+    }
+
+    public static String calculateSize(long size) {
+        char[] multipliers = {'B', 'K', 'M', 'G', 'T'};
+        for (int i = 0; i < multipliers.length; i++) {
+            double value = size / Math.pow(1024, i);
+            if (value < 1024) {
+                return "" + Math.round(value) + multipliers[i] + (i > 0 ? "b" : "");
+            }
+        }
+        return "Enchantment";
     }
 }
--- FilesAndNetwork/CountFileSizes/src/main/java/Main.java
+++ FilesAndNetwork/CountFileSizes/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
+                long sizeAllFile = FileUtils.calculateFolderSize(input);
+                String output = FileUtils.calculateSize(sizeAllFile);
+                System.out.println(output);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
