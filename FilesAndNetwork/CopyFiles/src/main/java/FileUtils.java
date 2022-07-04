68cf3bd4767730de7c5efe0be47274068adf1151
Kirill Alexeichik <Kira_alx@mail.ru>
2022-07-04 20:39:34 +0300

    Done exercise CopyFiles

--- FilesAndNetwork/CopyFiles/src/main/java/FileUtils.java
+++ FilesAndNetwork/CopyFiles/src/main/java/FileUtils.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        try {
+            File source = new File(sourceDirectory);
+            Path dest = Path.of(destinationDirectory);
+            File[] folder = source.listFiles();
+
+            if (folder != null) {
+                for (File file : folder) {
+                    Files.copy(file.toPath(), dest.resolve(file.getName()));
+                    copyFolder(file.getAbsolutePath(), dest.resolve(file.getName()).toString());
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- FilesAndNetwork/CopyFiles/src/main/java/Main.java
+++ FilesAndNetwork/CopyFiles/src/main/java/Main.java
@@ -1,4 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String textErr = "Вы неверно ввели путь, повторите попытку" + "\n";
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите путь папки которую нужно скопировать: ");
+            String source = scanner.nextLine();
+            File file = new File(source);
+            if (!file.exists()) {
+                System.out.println(textErr);
+                continue;
+            }
+            System.out.println("Введите путь целевой папки: ");
+            String destination = scanner.nextLine();
+            File file1 = new File(destination);
+            if (!file1.exists()) {
+                System.out.println(textErr);
+                continue;
+            }
+            FileUtils.copyFolder(source, destination);
+            System.out.println("Копирование выполнено");
+            System.out.println("");
+        }
     }
 }
