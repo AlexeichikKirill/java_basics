bfea693d265c44a821b0315f8af857a296a9d93c
Kirill Alexeichik <Kira_alx@mail.ru>
2022-09-19 19:00:10 +0300

    Done homework 19.14

--- SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoListController.java
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoListController.java
@@ -1,30 +1,48 @@
 package main;
 
+import main.model.ToDoListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import main.model.ToDoList;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class ToDoListController {
 
+    @Autowired
+    private ToDoListRepository toDoListRepository;
+
     @GetMapping("/ToDoLists/")
     public List<ToDoList> getToDoLists() {
-        return StorageToDoList.getAllToDoLists();
+        Iterable<ToDoList> toDoListIterable = toDoListRepository.findAll();
+        List<ToDoList> lists = new ArrayList<>();
+        for (ToDoList toDoList : toDoListIterable) {
+            lists.add(toDoList);
+        }
+        return lists;
     }
 
     @GetMapping("/ToDoLists/{id}")
     public ToDoList getToDoList(@PathVariable int id) {
-        return StorageToDoList.get(id);
+        Optional<ToDoList> optional = toDoListRepository.findById(id);
+        if (!optional.isPresent()){
+            return null;
+        }
+        return optional.get();
     }
 
     @PostMapping("/ToDoLists/")
     public int addToDoList(ToDoList toDoList) {
-        return StorageToDoList.addToDoList(toDoList);
+        ToDoList tds = toDoListRepository.save(toDoList);
+        return tds.getId();
     }
 
     @PutMapping("/ToDoLists/{id}")
     public ToDoList putToDoList(@PathVariable int id, String message, String authorName) {
-        ToDoList toDoList = StorageToDoList.get(id);
+        ToDoList toDoList = toDoListRepository.findById(id).get();
         if (message.length() > 0) {
             toDoList.setMessage(message);
         }
@@ -36,19 +54,19 @@ public class ToDoListController {
 
     @PutMapping("/ToDoList/")
     public List<ToDoList> putToDoLists(@RequestBody List<ToDoList> list) {
-        StorageToDoList.deleteAllToDoLists();
-        list.forEach(StorageToDoList::addToDoList);
-        return StorageToDoList.getAllToDoLists();
+        Iterable<ToDoList> iterable = list;
+        toDoListRepository.saveAll(iterable);
+        return list;
     }
 
     @DeleteMapping("/ToDoLists/{id}")
     public void delete(@PathVariable int id) {
-        StorageToDoList.deleteToDoList(id);
+        toDoListRepository.deleteById(id);
     }
 
     @DeleteMapping("/ToDoLists/")
     public void deleteAllToDoLists() {
-        StorageToDoList.deleteAllToDoLists();
+        toDoListRepository.deleteAll();
     }
 
 }
--- SpringBootWebDevelopment/ToDoList/src/main/java/main/model/ToDoList.java
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/model/ToDoList.java
@@ -5,8 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class ToDoList {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String authorName;
