bd5105f663d432c89c62cfde5cb7e85bc45e0f08
Kirill Alexeichik <Kira_alx@mail.ru>
2022-09-20 10:28:57 +0300

    Done homework 19.16

--- SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,14 +1,27 @@
 package main;
 
+import main.model.ToDoList;
+import main.model.ToDoListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-@RestController
+@Controller
 public class DefaultController {
+
+    @Autowired
+    private ToDoListRepository toDoListRepository;
+
     @RequestMapping("/")
-    public String index() {
-        return (new Date()).toString();
+    public String index(Model model) {
+        Iterable<ToDoList> iterable = toDoListRepository.findAll();
+        ArrayList<ToDoList> list = new ArrayList<>();
+        iterable.forEach(list::add);
+        model.addAttribute("tDLs", list);
+        return "index";
     }
 }
