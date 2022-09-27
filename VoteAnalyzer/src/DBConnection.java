88f8039f3d69ddfdbd121ef9054355e952164d67
Kirill Alexeichik <Kira_alx@mail.ru>
2022-09-27 15:06:19 +0300

    Done homework 22.20

--- VoteAnalyzer/src/DBConnection.java
+++ VoteAnalyzer/src/DBConnection.java
@@ -1,28 +1,29 @@
 import java.sql.*;
 
-public class DBConnection
-{
+public class DBConnection {
     private static Connection connection;
 
-    private static String dbName = "learn";
-    private static String dbUser = "root";
-    private static String dbPass = "ya78yrc8n4w3984";
+    private static final String dbName = "learn";
+    private static final String dbUser = "root";
+    private static final String dbPass = "6Jgfj2NO";
+    private static int num = 0;
 
-    public static Connection getConnection()
-    {
-        if(connection == null)
-        {
+    private static StringBuilder insertQuery = new StringBuilder();
+
+    public static Connection getConnection() {
+        if (connection == null) {
             try {
                 connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/" + dbName +
-                    "?user=" + dbUser + "&password=" + dbPass);
+                        "jdbc:mysql://localhost:3306/" + dbName +
+                                "?user=" + dbUser + "&password=" + dbPass);
                 connection.createStatement().execute("DROP TABLE IF EXISTS voter_count");
                 connection.createStatement().execute("CREATE TABLE voter_count(" +
                         "id INT NOT NULL AUTO_INCREMENT, " +
                         "name TINYTEXT NOT NULL, " +
                         "birthDate DATE NOT NULL, " +
                         "`count` INT NOT NULL, " +
-                        "PRIMARY KEY(id))");
+                        "PRIMARY KEY(id)," +
+                        "UNIQUE KEY name_date(name(50), birthDate))");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -30,31 +31,33 @@ public class DBConnection
         return connection;
     }
 
-    public static void countVoter(String name, String birthDay) throws SQLException
-    {
-        birthDay = birthDay.replace('.', '-');
-        String sql = "SELECT id FROM voter_count WHERE birthDate='" + birthDay + "' AND name='" + name + "'";
-        ResultSet rs = DBConnection.getConnection().createStatement().executeQuery(sql);
-        if(!rs.next())
-        {
-            DBConnection.getConnection().createStatement()
-                    .execute("INSERT INTO voter_count(name, birthDate, `count`) VALUES('" +
-                            name + "', '" + birthDay + "', 1)");
-        }
-        else {
-            Integer id = rs.getInt("id");
-            DBConnection.getConnection().createStatement()
-                    .execute("UPDATE voter_count SET `count`=`count`+1 WHERE id=" + id);
-        }
-        rs.close();
+    public static void executeMultiInsert() throws SQLException {
+        String sql = "INSERT INTO voter_count(name, birthDate, `count`) " +
+                "VALUES" + insertQuery.toString() +
+                "ON DUPLICATE KEY UPDATE `count`=`count` + 1";
+        DBConnection.getConnection().createStatement().execute(sql);
     }
 
-    public static void printVoterCounts() throws SQLException
-    {
+    public static void countVoter(String name, String birthDay) throws SQLException, InterruptedException {
+        birthDay = birthDay.replace('.', '-');
+        int limit = 1_000_000;
+
+        insertQuery.append(insertQuery.length() == 0 ? "" : ",")
+                .append("('").append(name).append("', '").append(birthDay).append("', 1)");
+        num++;
+
+        if (num % limit == 0) {
+            executeMultiInsert();
+            insertQuery = new StringBuilder();
+            System.out.println("+1_000_000");
+        }
+
+    }
+
+    public static void printVoterCounts() throws SQLException {
         String sql = "SELECT name, birthDate, `count` FROM voter_count WHERE `count` > 1";
         ResultSet rs = DBConnection.getConnection().createStatement().executeQuery(sql);
-        while(rs.next())
-        {
+        while (rs.next()) {
             System.out.println("\t" + rs.getString("name") + " (" +
                     rs.getString("birthDate") + ") - " + rs.getInt("count"));
         }
--- VoteAnalyzer/src/XMLHandler.java
+++ VoteAnalyzer/src/XMLHandler.java
@@ -1,41 +1,32 @@
 import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class XMLHandler extends DefaultHandler {
-    private Voter voter;
-    private static SimpleDateFormat birthDayFormat = new SimpleDateFormat("yyyy.MM.dd");
     private HashMap<Voter, Integer> voterCount;
 
-    public XMLHandler(){
+    public XMLHandler() {
         voterCount = new HashMap<>();
     }
 
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        try {
-            if (qName.equals("voter") & voter == null) {
-                Date birthDay = birthDayFormat.parse(attributes.getValue("birthDay"));
-                voter = new Voter(attributes.getValue("name"), birthDay);
-            } else if (qName.equals("visit") & voter != null){
-                int count = voterCount.getOrDefault(voter, 0);
-                voterCount.put(voter, count + 1);
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
+    private int limit = 5_000_000;
+    private int number = 0;
 
     @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-     if (qName.equals("voter")){
-         voter = null;
-     }
+    public void startElement(String uri, String localName, String qName, Attributes attributes){
+        if (qName.equals("voter") & number < limit) {
+            String name = attributes.getValue("name");
+            String birthDate = attributes.getValue("birthDay");
+
+            try {
+                DBConnection.countVoter(name, birthDate);
+            } catch (SQLException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     public void printDuplicatedVoters(){
